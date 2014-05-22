3277505bc950c0971c46912942b84fb4e98f6f21
Daniel Martí <mvdan@mvdan.cc>
2014-05-22 16:59:26 +0200

    Use Common.mk from Android.mk

--- Android.mk
+++ Android.mk
@@ -1,26 +1,9 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := \
-	bmx.c \
-	msg.c \
-	metrics.c \
-	tools.c \
-	plugin.c \
-	list.c \
-	allocate.c \
-	avl.c \
-	iid.c \
-	hna.c \
-	control.c \
-	schedule.c \
-	ip.c \
-	cyassl/sha.c \
-	cyassl/random.c \
-	cyassl/arc4.c
+include $(LOCAL_PATH)/Common.mk
 
-GIT_REV := $(shell ( [ "$(REVISION_VERSION)" ] && echo "$(REVISION_VERSION)" ) || ( [ -d .git ] && git --no-pager log -n 1 --oneline|cut -d " " -f 1 ) ||  echo 0)
-LOCAL_CFLAGS := -Wall -W -Wno-unused-parameter -O0 -g3 -std=gnu99 -DGIT_REV=\"$(GIT_REV)\"
+LOCAL_SRC_FILES := $(SRC_C)
 
 # -pedantic yields a lot of warnings from the NDK includes
 LOCAL_CFLAGS := $(filter-out -pedantic,$(CFLAGS))
